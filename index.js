2b160859f7e185cc7332fc6c3cf7c435db12cbe0
RemcoE33 <remco.edelenbos@gmail.com>
2021-12-08 21:27:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,31 @@
 import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
-import axiod from "https://deno.land/x/axiod/mod.ts";
+
+const postRequest = await fetch("https://stockx.com/p/e", {
+  method: "post",
+  headers: {
+    "apollographql-client-name": "Iron",
+    "apollographql-client-version": "2021.12.01",
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({
+    operationName: "ProductList",
+    variables: {
+      country: "US",
+      currencyCode: "USD",
+      limit: 500,
+      type: "MOST_POPULAR",
+      vertical: "SNEAKERS",
+    },
+    query:
+      "query ProductList($country: String!, $currencyCode: CurrencyCode!, $marketName: String, $type: ProductSelectionType!, $limit: Int!, $vertical: Vertical) {  allProducts(    filters: {vertical: {is: $vertical}}    type: $type    page: {page: 0, limit: $limit}  ) {    ...ProductListFragment    __typename  }}fragment ProductListFragment on ProductsConnection {  edges {    node {      title      productCategory      name      urlKey      uuid      media {        smallImageUrl        __typename      }      market(currencyCode: $currencyCode) {        bidAskData(country: $country, market: $marketName) {          highestBid          lastHighestBidTime          lastLowestAskTime          lowestAsk          __typename        }        salesInformation {          lastSale          salesThisPeriod          __typename        }        __typename      }      traits(filterTypes: [RELEASE_DATE]) {        name        value        __typename      }      __typename    }    __typename  }  __typename}",
+  }),
+});
+
+const data = await postRequest.json();
+console.log(data)
 
 serve((_req) => {
-  return new Response(
-    axiod({
-      method: "post",
-      url: "https://stockx.com/p/e",
-      headers: {
-        "apollographql-client-name": "Iron",
-        "apollographql-client-version": "2021.12.01",
-        "Content-Type": "application/json",
-      },
-      data: JSON.stringify({
-        operationName: "ProductList",
-        variables: {
-          country: "US",
-          currencyCode: "USD",
-          limit: 500,
-          type: "MOST_POPULAR",
-          vertical: "SNEAKERS",
-        },
-        query:
-          "query ProductList($country: String!, $currencyCode: CurrencyCode!, $marketName: String, $type: ProductSelectionType!, $limit: Int!, $vertical: Vertical) {  allProducts(    filters: {vertical: {is: $vertical}}    type: $type    page: {page: 0, limit: $limit}  ) {    ...ProductListFragment    __typename  }}fragment ProductListFragment on ProductsConnection {  edges {    node {      title      productCategory      name      urlKey      uuid      media {        smallImageUrl        __typename      }      market(currencyCode: $currencyCode) {        bidAskData(country: $country, market: $marketName) {          highestBid          lastHighestBidTime          lastLowestAskTime          lowestAsk          __typename        }        salesInformation {          lastSale          salesThisPeriod          __typename        }        __typename      }      traits(filterTypes: [RELEASE_DATE]) {        name        value        __typename      }      __typename    }    __typename  }  __typename}",
-      }),
-    }).then((response) => {
-      return JSON.stringify(response.data);
-    }),
-    {
-      headers: { "content-type": "application/json" },
-    }
-  );
+  return new Response(JSON.stringify(data), {
+    headers: { "content-type": "application/json" },
+  });
 });
